35f96e87dc9a4512ce5d147e44470ee45a6bc88e
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2022-12-10 17:53:15 -0300

    requisito 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ class Header extends Component {
   render() {
     return (
       <div>Header</div>
-      // iniciando o projeto
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    buttonDisabled: true,
+    email: '',
+    password: '',
+  };
+
+  handleChangeButton = () => {
+    const { email, password } = this.state;
+    const user = /\S+@\S+\.\S+/;
+    const minChars = 6;
+    const validEmail = user.test(email);
+    const validPass = password.length >= minChars;
+
+    if (validEmail && validPass) {
+      this.setState({
+        buttonDisabled: false });
+    } else {
+      this.setState({
+        buttonDisabled: true,
+      });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.handleChangeButton);
+  };
+
   render() {
-    return <div>Login</div>;
+    const { buttonDisabled, email, password } = this.state;
+    const { dispatch, history } = this.props;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email-input">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={ email }
+              data-testid="email-input"
+              placeholder="E-mail"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password-input">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={ password }
+              data-testid="password-input"
+              placeholder="Password"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ buttonDisabled }
+            onClick={ () => dispatch(emailAction(email)) && history.push('/carteira') }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
