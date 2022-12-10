2714b1cf340c81e75ff19d80d6dbc9f48cd5222c
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2022-12-10 18:06:44 -0300

    corrigindo requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { emailAction } from '../redux/actions';
 
 class Login extends Component {
   state = {
